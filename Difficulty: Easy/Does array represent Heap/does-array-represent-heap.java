db349448309715c13321c2c09f174879108313a6
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2025-07-17 14:59:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Does array represent Heap/does-array-represent-heap.java
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.java
@@ -6,7 +6,7 @@ class Solution {
         // Your code goes here
        for(int i=0; i<arr.length; i++){
            int left= 2*i+1;
-           int right=2*i+2;
+           int right= 2*i+2;
            if(left<n && arr[left]>arr[i] || right<n && arr[right]>arr[i]){
                return false;
            }
