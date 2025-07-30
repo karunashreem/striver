0e9b7f840cda21b729c6c34b3bc1f3a0dda9f5b0
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2025-07-30 19:04:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Minimum element in BST/minimum-element-in-bst.java
@@ -0,0 +1,23 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+class Solution {
+    // Function to find the minimum element in the given BST.
+    int minValue(Node root) {
+        // code here
+        Node temp= root;
+        while(temp.left!=null){
+            temp=temp.left;
+        }
+        return temp.data;
+    }
+}
