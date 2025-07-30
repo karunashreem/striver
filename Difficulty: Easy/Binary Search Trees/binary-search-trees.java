42d8f13fb7b2e4a3b3b15cc0cc03f3778acc3706
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2025-07-30 18:58:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search Trees/binary-search-trees.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    static boolean isBSTTraversal(int arr[]) {
+        // code here
+        for(int i=0; i<arr.length-1; i++){
+            if(arr[i]>=arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
