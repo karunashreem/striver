c0270865c14dbf42d1e52275253398b7f032be18
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2025-08-09 16:10:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    void printNos(int N) {
+        // code here
+        if(N==1){
+            System.out.print( N+" ");
+            return;
+        }
+        System.out.print(N+" ");
+        printNos(N-1);
+    }
+}
