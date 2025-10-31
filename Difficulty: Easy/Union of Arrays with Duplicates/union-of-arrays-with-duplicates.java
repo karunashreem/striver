2d5c06bc1b2c9089eff1c366937a5803a43ea56c
M Karuna Shree <127692384+karunashreem@users.noreply.github.com>
2025-11-01 03:58:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        // code here
+        TreeSet<Integer> set= new TreeSet<>();
+        for(int n: a){
+            set.add(n);
+        }
+        for(int n: b){
+            set.add(n);
+        }
+        ArrayList<Integer> union= new ArrayList<>(set);
+        return union;
+    }
+}
